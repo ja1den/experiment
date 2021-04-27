4e23dcd7f1798505bde6c9f412d2d93f0b5ffbf6
Jaiden Douglas <49811314+ja1den@users.noreply.github.com>
2021-04-27 17:13:10 +0930

    feat: add table output

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import fs from 'fs';
 
 import { spawnSync } from 'child_process';
+import readline from 'readline';
 
 import 'colors';
 
@@ -42,18 +43,63 @@ async function main() {
 		// Import Scenario
 		const { default: scenario }: { default: Scenario } = await import(path.resolve(__dirname, 'scenarios', name));
 
-		// Log Name
-		if (scenario.name === 'File I/O') continue;
-
+		// Scenario Name
 		console.log((' ' + scenario.name + ' ').black.bgGreen + '\n');
+
+		// Loading
+		let step = 1;
+
+		const loadInterval = setInterval(() => {
+			readline.cursorTo(process.stdout, 0);
+			process.stdout.write('.'.repeat(step++ % 4).padEnd(3, ' ').cyan);
+		}, 400);
 
 		// Run Scenario
 		const data = await scenario.call();
 
 		data.js.bundle += size;
 
-		// Log Data
-		console.log(JSON.stringify(data) + '\n');
+		// Loading
+		readline.cursorTo(process.stdout, 0);
+		clearInterval(loadInterval);
+
+		// Table Data
+		const columns: ([string, (string | number)[]])[] = [];
+
+		columns.push(['Language', ['JavaScript', 'Rust']]);
+
+		for (let i = 0; i < Math.max(data.js.results.length, data.rs.results.length); i++) {
+			columns.push(['T' + (i + 1) + ' (sec)', [data.js.results[i]?.duration ?? '', data.rs.results[i]?.duration ?? '']]);
+			columns.push(['M' + (i + 1) + ' (kB)', [data.js.results[i]?.memory ?? '', data.rs.results[i]?.memory ?? '']]);
+		}
+
+		columns.push(['Bundle (B)', [data.js.bundle, data.rs.bundle]]);
+
+		// Parse Data
+		const parsed: string[][] = [];
+
+		for (const column of columns) {
+			// Length
+			const length = column[1].reduce((length: number, entry) => Math.max(entry.toString().length, length), Math.max(column[0].length, 8));
+
+			// Title
+			parsed[0] = [...(parsed[0] ?? []), column[0].padEnd(length, ' ')];
+			parsed[1] = [...(parsed[1] ?? []), '-'.repeat(length)];
+
+			// Entries
+			for (let i = 0; i < column[1].length; i++) {
+				parsed[i + 2] = [...(parsed[i + 2] ?? []), !isNaN(parseInt(column[1][i].toString()))
+					? column[1][i].toString().padEnd(length, ' ').yellow
+					: column[1][i].toString().padEnd(length, ' ')
+				];
+			}
+		}
+
+		// Log Table
+		for (let i = 0; i < parsed.length; i++) console.log('| ' + parsed[i].join(' | ') + ' |');
+
+		// Log Newline
+		if (name !== names[names.length - 1]) console.log('');
 	}
 }
 main();
