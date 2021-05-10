c52ea4451627154b9cb7f1a1d4541aef075f3eaf
Jaiden Douglas <49811314+ja1den@users.noreply.github.com>
2021-05-10 22:26:30 +0930

    style: update formatting

--- scenarios/1/rs/src/main.rs
+++ scenarios/1/rs/src/main.rs
@@ -3,42 +3,42 @@ use std::{env, fs, io};
 
 // Main Function
 fn main() {
-    // Resolve Directories
-    let root = env::current_dir()
-        .unwrap()
-        .join("..")
-        .canonicalize()
-        .unwrap();
+	// Resolve Directories
+	let root = env::current_dir()
+		.unwrap()
+		.join("..")
+		.canonicalize()
+		.unwrap();
 
-    let i_path = root.join("data/input").canonicalize().unwrap();
-    let o_path = root.join("data/rs").canonicalize().unwrap();
+	let i_path = root.join("data/input").canonicalize().unwrap();
+	let o_path = root.join("data/rs").canonicalize().unwrap();
 
-    // File Names
-    let mut names = fs::read_dir(i_path)
-        .unwrap()
-        .map(|res| res.map(|e| e.path()))
-        .collect::<Result<Vec<_>, io::Error>>()
-        .unwrap();
+	// File Names
+	let mut names = fs::read_dir(i_path)
+		.unwrap()
+		.map(|res| res.map(|e| e.path()))
+		.collect::<Result<Vec<_>, io::Error>>()
+		.unwrap();
 
-    names.sort();
+	names.sort();
 
-    // Read Files
-    for name in names {
-        // File Content
-        let read = fs::read_to_string(name.clone()).unwrap();
+	// Read Files
+	for name in names {
+		// File Content
+		let read = fs::read_to_string(name.clone()).unwrap();
 
-        // Parse and Sort Numbers
-        let mut numbers: Vec<u64> = read.lines().map(|s| s.parse::<u64>().unwrap()).collect();
-        numbers.sort();
+		// Parse and Sort Numbers
+		let mut numbers: Vec<u64> = read.lines().map(|s| s.parse::<u64>().unwrap()).collect();
+		numbers.sort();
 
-        // Stringify Numbers
-        let data: Vec<String> = numbers.iter().map(|n| format!("{:0>8}", n)).collect();
+		// Stringify Numbers
+		let data: Vec<String> = numbers.iter().map(|n| format!("{:0>8}", n)).collect();
 
-        // Create File
-        fs::write(o_path.join(name.file_name().unwrap()), data.join("\n")).unwrap();
+		// Create File
+		fs::write(o_path.join(name.file_name().unwrap()), data.join("\n")).unwrap();
 
-        // Sum Numbers
-        let sum: u64 = numbers.iter().sum();
-        println!("{}", sum);
-    }
+		// Sum Numbers
+		let sum: u64 = numbers.iter().sum();
+		println!("{}", sum);
+	}
 }
--- scenarios/2/rs/src/main.rs
+++ scenarios/2/rs/src/main.rs
@@ -3,45 +3,45 @@ use std::{env, io::prelude::*, net::TcpListener};
 
 // Main Function
 fn main() {
-    // Read Maximum
-    let args: Vec<String> = env::args().collect();
+	// Read Maximum
+	let args: Vec<String> = env::args().collect();
 
-    if args.len() != 2 {
-        println!("argument 'max' must be defined");
-        std::process::exit(1);
-    }
+	if args.len() != 2 {
+		println!("argument 'max' must be defined");
+		std::process::exit(1);
+	}
 
-    // Initialise Count
-    let max = match args[1].parse::<i32>() {
-        Ok(n) => n,
-        Err(_) => {
-            println!("argument 'max' must be an integer");
-            std::process::exit(1);
-        }
-    };
-    let mut i = 0;
+	// Initialise Count
+	let max = match args[1].parse::<i32>() {
+		Ok(n) => n,
+		Err(_) => {
+			println!("argument 'max' must be an integer");
+			std::process::exit(1);
+		}
+	};
+	let mut i = 0;
 
-    // Listen on 4000
-    let listener = TcpListener::bind("127.0.0.1:4000").unwrap();
+	// Listen on 4000
+	let listener = TcpListener::bind("127.0.0.1:4000").unwrap();
 
-    println!("http://localhost:4000");
+	println!("http://localhost:4000");
 
-    // Iterate on Requests
-    for stream in listener.incoming() {
-        let mut stream = stream.unwrap();
+	// Iterate on Requests
+	for stream in listener.incoming() {
+		let mut stream = stream.unwrap();
 
-        // Increment
-        i = i + 1;
+		// Increment
+		i = i + 1;
 
-        // Send Response
-        stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
-        stream.flush().unwrap();
+		// Send Response
+		stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap();
+		stream.flush().unwrap();
 
-        println!("{}", i);
+		println!("{}", i);
 
-        // Check Maximum
-        if i == max {
-            break;
-        };
-    }
+		// Check Maximum
+		if i == max {
+			break;
+		};
+	}
 }
