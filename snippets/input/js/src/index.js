b98b7147d8afcb2739915f4bc42efff03fea781b
Jaiden Douglas <49811314+ja1den@users.noreply.github.com>
2021-05-17 21:11:52 +0930

    feat: add input snippets

---
+++ snippets/input/js/src/index.js
@@ -0,0 +1,8 @@
+// Read the input as a string.
+let input = process.argv[2];
+
+// Parse the string into a number.
+input = parseInt(input);
+
+// Log the number to to console.
+console.log(input);
---
+++ snippets/input/rs/src/main.rs
@@ -0,0 +1,14 @@
+// Import
+use std::env;
+
+// Main Function
+fn main() {
+	// Read the input as a string.
+	let input = &env::args().collect::<Vec<String>>()[1];
+
+	// Parse the string into a number.
+	let parsed_input: i32 = input.parse().unwrap();
+
+	// Log the number to the console.
+	println!("{}", parsed_input + 1);
+}
